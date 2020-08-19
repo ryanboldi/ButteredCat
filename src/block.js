012be34fa73eb9944071620a22310f601ff2b6c2
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-08-19 15:33:59 +0400

    neighbourPixel nearly implemented

--- src/block.js
+++ src/block.js
@@ -9,9 +9,25 @@ class Block {
         this.pixels = Array.from(Array(gridSize), () => new Array(gridSize))
         for (let i = 0; i < gridSize; i++) {
             for (let j = 0; j < gridSize; j++) {
-                this.pixels[i][j] = 1;
+                this.pixels[i][j] = 0;
             }
         }
+        this.pixels[1][3] = 1;
+        this.pixels[2][3] = 1;
+        this.pixels[3][3] = 1;
+        this.pixels[3][4] = 1;
+
+        this.vertices = [];
+        for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
+
+            }
+        }
+
+
+
+
+
     }
 
 
@@ -25,6 +41,32 @@ class Block {
                 rectMode(CORNER);
                 rect(this.x + (i * this.smallWidth), this.y + (j * this.smallWidth), this.smallWidth, this.smallWidth);
                 pop();
+            }
+        }
+    }
+
+    /**
+     * returns true if there is a neighbor to pixel at x,y in direction dir, else false;
+     * @param {number} x x coord of the pixel
+     * @param {number} y y coord of the pixel
+     * @param {number} dir 0 for N, 1 for E, 2 for S, 3 for W
+     */
+    neighbourPixel(x, y, dir) {
+        // if x, y is on the board
+        if (x < gridSize && y < gridSize && x >= 0 && y >= 0) {
+
+
+            //check and make sure that x,y is not on a border, if so, no neighbors in that direction
+            if (x = gridSize - 1 && dir == 1) {
+                return false;
+            } else if (x = 0 && dir == 3) {
+                return false;
+            } else if (y = gridSize - 1 && dir == 2) {
+                return false;
+            } else if (y = 0 && dir == 0) {
+                return false;
+            } else {
+                return true;
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ let engine = Engine.create();
 
 let boxA = Bodies.rectangle(400, 200, 80, 80);
 let boxB = Bodies.rectangle(400, 50, 80, 80);
-let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+let ground = Bodies.rectangle(400, 770, 810, 60, { isStatic: true });
 
 World.add(engine.world, [boxA, boxB, ground]);
 
@@ -36,6 +36,15 @@ function draw() {
     });
     endShape(CLOSE);
     b.draw();
+
+    push();
+    fill(50, 60, 70);
+    beginShape();
+    for (let i = 0; i < ground.vertices.length; i++) {
+        vertex(ground.vertices[i].x, ground.vertices[i].y);
+    }
+    endShape(CLOSE);
+    pop();
 }
 
 
